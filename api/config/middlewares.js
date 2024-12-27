907286b1f2a72cbbe0a0ea7efe0e3ddb577e9eda
hiago-miguel <miguel.hiago@gmail.com>
2024-12-27 00:04:22 -0300

    add toml

--- api/config/middlewares.js
+++ api/config/middlewares.js
@@ -2,7 +2,16 @@ module.exports = [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      origin: ['https://hiago-journey-nextjs.netlify.app'], // Replace with your actual Next.js app URL
+      methods: ['GET', 'POST', 'PUT', 'DELETE'],
+      allowedHeaders: ['Content-Type', 'Authorization'],
+      credentials: true, // Optional, depending on your setup
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
